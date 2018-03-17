0eca6b502e377bc882ce3aa0fe832dfa4bfb6f6a
redstoone <cy_jing_2010@163.com>
2018-03-17 10:13:50 +0800

    添加element-ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Login from '@/views/login'
 
 Vue.use(Router)
 
 export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({
+    y: 0
+  }),
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
     }
   ]
 })

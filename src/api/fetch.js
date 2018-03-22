83cff11065c8b6999abb4f68e1279348f1333c0f
redstoone <cy_jing_2010@163.com>
2018-03-23 01:07:04 +0800

    修改

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -7,7 +7,7 @@ const service = function (url, data, method = 'GET', headers = {}) {
   return new Promise((resolve, reject) => {
     let options = {
       'method': method,
-      'url': 'http://121.196.208.151:9000/' + process.env.BASE_API + url,
+      'url': 'http://121.196.208.151:9000' + process.env.BASE_API + url,
       'headers': headers && typeof headers === 'object' ? headers : {}
     }
     options.headers['X-Requested-Page'] = 'json'

5bbfee4149f6a9b5dc947e0311c2b4eceaa4fedf
redstoone <cy_jing_2010@163.com>
2018-03-23 01:04:49 +0800

    修改

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,12 +3,11 @@ import { Message } from 'element-ui'
 // import store from '@/store'
 // import { getToken } from '@/utils/auth'
 
-console.log(process.env)
 const service = function (url, data, method = 'GET', headers = {}) {
   return new Promise((resolve, reject) => {
     let options = {
       'method': method,
-      'url': process.env.BASE_API + url,
+      'url': 'http://121.196.208.151:9000/' + process.env.BASE_API + url,
       'headers': headers && typeof headers === 'object' ? headers : {}
     }
     options.headers['X-Requested-Page'] = 'json'
